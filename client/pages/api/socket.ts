8c97b3d834d6d0b00b05f0da1e28e98150aabf77
M Fazeel <mujaddadfazeel@gmail.com>
2023-05-17 03:40:48 -0500

    testing server

--- client/pages/api/socket.ts
+++ client/pages/api/socket.ts
@@ -1,75 +1,23 @@
-import { io, Socket } from "socket.io-client";
+import { VercelRequest, VercelResponse } from '@vercel/node'
+import { Server } from 'ws'
 
-const EVENTS = {
-    connection: "connection",
-    CLIENT: {
-        CREATE_ROOM: "CREATE_ROOM",
-        DELETE_ROOM: "DELETE_ROOM", // New Event for Deleting Room
-        SEND_ROOM_MESSAGE: "SEND_ROOM_MESSAGE",
-        JOIN_ROOM: "JOIN_ROOM",
-        GET_ROOMS: "GET_ROOMS",
-        GET_ROOM_HISTORY: "GET_ROOM_HISTORY", // New event to get room history
-    },
-    SERVER: {
-        ROOMS: "ROOMS",
-        JOINED_ROOM: "JOINED_ROOM",
-        ROOM_MESSAGE: "ROOM_MESSAGE",
-        ROOM_HISTORY: "ROOM_HISTORY", // New event to send room history
-    },
-};
+const wss = new Server({ noServer: true })
 
-let socket: Socket;
+wss.on('connection', (ws) => {
+  ws.on('message', (message) => {
+    console.log('received: %s', message)
+  })
 
-function connect() {
-    socket = io('https://chat-app-m-fazeel.vercel.app/'); // Update this URL to your server's URL
+  ws.send('something from server')
+})
 
-    socket.on(EVENTS.SERVER.JOINED_ROOM, (roomId) => {
-        console.log(`Joined room: ${roomId}`);
-    });
+module.exports = (req: VercelRequest, res: VercelResponse) => {
+  if (!('upgrade' in req.headers)) {
+    return res.status(426).send('Expected an upgrade header')
+  }
 
-    socket.on(EVENTS.SERVER.ROOMS, (rooms) => {
-        console.log('Rooms:', rooms);
-    });
-
-    socket.on(EVENTS.SERVER.ROOM_MESSAGE, (message) => {
-        console.log('New message:', message);
-    });
-
-    socket.on(EVENTS.SERVER.ROOM_HISTORY, (history) => {
-        console.log('Room history:', history);
-    });
-}
-
-function getRooms() {
-    socket.emit(EVENTS.CLIENT.GET_ROOMS);
-}
-
-function createRoom(roomName: string) {
-    socket.emit(EVENTS.CLIENT.CREATE_ROOM, roomName);
-}
-
-function deleteRoom(roomId: string) {
-    socket.emit(EVENTS.CLIENT.DELETE_ROOM, roomId);
-}
-
-function joinRoom(roomId: string) {
-    socket.emit(EVENTS.CLIENT.JOIN_ROOM, roomId);
-}
-
-function getRoomHistory(roomId: string) {
-    socket.emit(EVENTS.CLIENT.GET_ROOM_HISTORY, roomId);
-}
-
-function sendMessage({ message, roomId, username }: { message: string; roomId: string; username: string; }) {
-    socket.emit(EVENTS.CLIENT.SEND_ROOM_MESSAGE, { message, roomId, username });
-}
-
-export default {
-    connect,
-    getRooms,
-    createRoom,
-    deleteRoom,
-    joinRoom,
-    getRoomHistory,
-    sendMessage
+  // handle as WebSocket connection
+  wss.handleUpgrade(req, req.socket, Buffer.alloc(0), (ws) => {
+    wss.emit('connection', ws, req)
+  })
 }
